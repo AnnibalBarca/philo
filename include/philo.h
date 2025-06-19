b6a357153ca9e008aa79a5e5c38ea6d115e8f2f5
Alexandre Meekel-Konjikovic <almeekel@z4r12p1.42lyon.fr>
2025-06-19 14:18:42 +0200

    init completed

--- include/philo.h
+++ include/philo.h
@@ -6,60 +6,63 @@
 /*   By: almeekel <almeekel@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/15 18:32:53 by almeekel          #+#    #+#             */
-/*   Updated: 2025/06/15 18:43:19 by almeekel         ###   ########.fr       */
+/*   Updated: 2025/06/15 19:44:48 by almeekel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_H
-#define PHILO_H
+# define PHILO_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <pthread.h>
-#include <sys/time.h>
-#include <string.h>
+# include <pthread.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <sys/time.h>
+# include <unistd.h>
 
-#define THINKING 0
-#define EATING 1
-#define SLEEPING 2
+# define THINKING 0
+# define EATING 1
+# define SLEEPING 2
 
-#define FORK_MSG "has taken a fork"
-#define EAT_MSG "is eating"
-#define SLEEP_MSG "is sleeping"
-#define THINK_MSG "is thinking"
-#define DEATH_MSG "died"
+# define FORK_MSG "has taken a fork"
+# define EAT_MSG "is eating"
+# define SLEEP_MSG "is sleeping"
+# define THINK_MSG "is thinking"
+# define DEATH_MSG "died"
 
-typedef struct s_data t_data;
+typedef struct s_data	t_data;
 
 typedef struct s_philo
 {
-	int phi_id;
-	int meals_already_eaten;
-	long last_meal_time;
-	pthread_t thread;
-	pthread_mutex_t *left_fork;
-	pthread_mutex_t *right_fork;
-	t_data *data;
-} t_philo;
+	int					phi_id;
+	int					state;
+	int					meals_already_eaten;
+	long				last_meal_time;
+	pthread_t			thread;
+	pthread_mutex_t		*left_fork;
+	pthread_mutex_t		*right_fork;
+	t_data				*data;
+}						t_philo;
 
 typedef struct s_data
 {
-	int num_of_phis;
-	int time_to_die;
-	int time_to_eat;
-	int time_to_sleep;
-	int num_of_meals;
-	int is_running;
-	long start_time;
-	pthread_mutex_t *forks;
-	pthread_mutex_t print_mutex;
-	pthread_mutex_t data_mutex;
-	t_philo *philos;
-} t_data;
+	int					num_of_phis;
+	int					time_to_die;
+	int					time_to_eat;
+	int					time_to_sleep;
+	int					num_of_meals;
+	int					is_running;
+	long				start_time;
+	pthread_mutex_t		*forks;
+	pthread_mutex_t		print_mutex;
+	pthread_mutex_t		data_mutex;
+	t_philo				*philos;
+}						t_data;
 
-int	data_init(t_data *data, int ac, char **av);
+int						data_init(t_data *data, int ac, char **av);
 
-
+int						cleanup_mutexes_init(t_data *data,
+							int forks_initialized, int print_initialized,
+							int data_initialized);
 
 #endif
--- src/init.c
+++ src/init.c
@@ -6,11 +6,11 @@
 /*   By: almeekel <almeekel@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/15 18:36:25 by almeekel          #+#    #+#             */
-/*   Updated: 2025/06/15 18:57:21 by almeekel         ###   ########.fr       */
+/*   Updated: 2025/06/19 14:18:05 by almeekel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "philo.h"
+#include "include/philo.h"
 
 int	data_init(t_data *data, int ac, char **av)
 {
@@ -23,7 +23,59 @@ int	data_init(t_data *data, int ac, char **av)
 	else
 		data->num_of_meals = -1;
 	data->is_running = 1;
-	return(1);
+	return (1);
 }
 
-int	init_mutexes()
+int	init_mutexes(t_data *data)
+{
+	int i;
+
+	data->forks = malloc(sizeof(pthread_mutex_t) * data->num_of_phis);
+	if (!data->forks)
+		return (0);
+	i = 0;
+	while (i < data->num_of_phis)
+	{
+		if (pthread_mutex_init(&data->forks[i], NULL) != 0)
+			return (cleanup_mutexes_partial(data, i, 0, 0));
+		i++;
+	}
+	if (pthread_mutex_init(&data->print_mutex, NULL) != 0)
+		return (cleanup_mutexes_partial(data, data->num_of_phis, 0, 0));
+	if (pthread_mutex_init(&data->data_mutex, NULL) != 0)
+		return (cleanup_mutexes_partial(data, data->num_of_phis, 1, 0));
+	return (1);
+}
+static void	assign_forks(t_philo *philo, t_data *data, int i)
+{
+	if (i == 0)
+	{
+		philo->left_fork = &data->forks[data->num_of_phis - 1];
+		philo->right_fork = &data->forks[i];
+	}
+	else
+	{
+		philo->left_fork = &data->forks[i - 1];
+		philo->right_fork = &data->forks[i];
+	}
+}
+int	init_philosophers(t_data *data)
+{
+	int	i;
+
+	data->philos = malloc(sizeof(t_philo) * data->num_of_phis);
+	if (!data->philos)
+		return (0);
+	i = 0;
+	while (i < data->num_of_phis)
+	{
+		data->philos[i].phi_id = i + 1;
+		data->philos[i].state = THINKING;
+		data->philos[i].meals_already_eaten = 0;
+		data->philos[i].last_meal_time = 0;
+		data->philos[i].data = data;
+		assign_forks(&data->philos[i], data, i);
+		i++;
+	}
+	return (1);
+}
