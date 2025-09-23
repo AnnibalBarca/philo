d16d4a12c84f6738268009b6d06a461fff6e0fb7
Alexandre Meekel-Konjikovic <almeekel@z3r6p6.42lyon.fr>
2025-09-23 18:14:56 +0200

    cleanup function for the mutexes using mutex destroy

--- src/cleanup.c
+++ src/cleanup.c
@@ -3,13 +3,48 @@
 /*                                                        :::      ::::::::   */
 /*   cleanup.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: almeekel <almeekel@student.42lyon.fr>      +#+  +:+       +#+        */
+/*   By: almeekel <almeekel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/15 19:02:47 by almeekel          #+#    #+#             */
-/*   Updated: 2025/06/15 19:36:33 by almeekel         ###   ########.fr       */
+/*   Updated: 2025/09/23 18:10:21 by almeekel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "include/philo.h"
+#include "philo.h"
+
+int	cleanup_mutexes_partial(t_data *data, int forks_initialized,
+		int print_initialized, int data_initialized)
+{
+	int	i;
+
+	i = 0;
+	while (i < forks_initialized)
+	{
+		pthread_mutex_destroy(&data->forks[i]);
+		i++;
+	}
+	if (print_initialized)
+		pthread_mutex_destroy(&data->print_mutex);
+	if (data_initialized)
+		pthread_mutex_destroy(&data->data_mutex);
+	free(data->forks);
+	return (0);
+}
+
+void	cleanup_all(t_data *data)
+{
+	int	i;
+
+	i = 0;
+	while (i < data->num_of_phis)
+	{
+		pthread_mutex_destroy(&data->forks[i]);
+		i++;
+	}
+	pthread_mutex_destroy(&data->print_mutex);
+	pthread_mutex_destroy(&data->data_mutex);
+	free(data->forks);
+	free(data->philos);
+}
 
 
