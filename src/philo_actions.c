c53e3aa0d18c7e33669d3b7bebd7e7e8c258dacb
Alexandre Meekel-Konjikovic <almeekel@z3r2p6.42lyon.fr>
2025-09-28 12:08:11 +0200

    Fix resource hierarchy and timing logic for Dining Philosophers
    - Implement proper resource hierarchy (lower fork index first)
    - Fix last_meal_time to be set after eating completes
    - Prevent deadlocks while allowing starvation with tight timing
    - Update README with current status and timing guidelines

--- src/philo_actions.c
+++ src/philo_actions.c
@@ -14,18 +14,35 @@
 
 void	take_forks(t_philo *philo)
 {
-	if (philo->phi_id % 2 == 0)
+	int	fork1_idx;
+	int	fork2_idx;
+
+	// Get the actual fork indices by comparing memory addresses
+	// Find which fork has the lower index in the forks array
+	for (fork1_idx = 0; fork1_idx < philo->data->num_of_phis; fork1_idx++)
 	{
-		pthread_mutex_lock(philo->right_fork);
-		print_status(philo, FORK_MSG);
+		if (&philo->data->forks[fork1_idx] == philo->left_fork)
+			break;
+	}
+	for (fork2_idx = 0; fork2_idx < philo->data->num_of_phis; fork2_idx++)
+	{
+		if (&philo->data->forks[fork2_idx] == philo->right_fork)
+			break;
+	}
+
+	// Always take the lower-indexed fork first to prevent deadlock
+	if (fork1_idx < fork2_idx)
+	{
 		pthread_mutex_lock(philo->left_fork);
+		print_status(philo, FORK_MSG);
+		pthread_mutex_lock(philo->right_fork);
 		print_status(philo, FORK_MSG);
 	}
 	else
 	{
-		pthread_mutex_lock(philo->left_fork);
-		print_status(philo, FORK_MSG);
 		pthread_mutex_lock(philo->right_fork);
+		print_status(philo, FORK_MSG);
+		pthread_mutex_lock(philo->left_fork);
 		print_status(philo, FORK_MSG);
 	}
 }
@@ -39,9 +56,9 @@ void	put_forks(t_philo *philo)
 void	eat(t_philo *philo)
 {
 	print_status(philo, EAT_MSG);
+	usleep(philo->data->time_to_eat * 1000);
 	pthread_mutex_lock(&philo->mutex);
 	philo->last_meal_time = get_time();
-	usleep(philo->data->time_to_eat * 1000);
 	philo->meals_already_eaten++;
 	pthread_mutex_unlock(&philo->mutex);
 }
